25fb8fb4130f1f918c6755d21d4f8a68b7d39527
Martin Pes <pes@cngroup.dk>
2023-06-06 09:18:43 +0200

    Implemented storage blob

--- src/Common/StorageBlob.cs
+++ src/Common/StorageBlob.cs
@@ -1,9 +1,26 @@
-﻿namespace HealthChecksPrimer.Common;
+﻿using Azure.Storage.Blobs;
+using System.Text;
+using System.Text.Json;
+
+namespace HealthChecksPrimer.Common;
 
 public class StorageBlob : IStorage
 {
-    public Task SaveAsync(DateTimeOffset dateTime, string symbol, decimal price)
+    public StorageBlob(BlobServiceClient blobServiceClient)
     {
-        return Task.CompletedTask;
+        _blobServiceClient = blobServiceClient;
     }
+
+    public async Task SaveAsync(DateTimeOffset dateTime, string symbol, decimal price)
+    {
+        var json = new { DateTime = dateTime, Symbol = symbol, Price = price };
+        var ms = new MemoryStream(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(json)));
+        ms.Seek(0, SeekOrigin.Begin);
+
+        var containerClient = _blobServiceClient.GetBlobContainerClient("stocks");
+        var blobClient = containerClient.GetBlobClient($"{symbol}-{Guid.NewGuid()}.json");
+        await blobClient.UploadAsync(ms, overwrite: true);
+    }
+
+    private readonly BlobServiceClient _blobServiceClient;
 }
---
+++ src/Common/StorageDummy.cs
@@ -0,0 +1,6 @@
+﻿namespace HealthChecksPrimer.Common;
+
+public class StorageDummy : IStorage
+{
+    public Task SaveAsync(DateTimeOffset dateTime, string symbol, decimal price) => Task.CompletedTask;
+}
