ff8d429593bbb654dc1cacf7e86297932ceebb27
martin-pes <50988072+martin-pes@users.noreply.github.com>
2023-06-13 06:56:43 +0200

    Implemented and registered a simple health check. Exposed the health check on API (#4)
    Co-authored-by: Martin Pes <pes@cngroup.dk>

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,12 +1,17 @@
+using HealthChecksPrimer.Common.Health;
 using HealthChecksPrimer.Common.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddStockServices(builder.Configuration);
 
+builder.Services.AddHealthChecks()
+    .AddCheck<FooHealthCheck>("Foo check");
+
 var app = builder.Build();
 
 app.MapGet("/", () => $"Health Check Primer, environment={builder.Environment.EnvironmentName}");
 app.MapPost("/stock/{symbol}", (string symbol, IStockService stockService) => stockService.Process(symbol));
+app.MapHealthChecks("/health");
 
 app.Run();
