fbb855271f3f075afc5691f144d300c2380725a3
Martin Pes <pes@cngroup.dk>
2023-06-13 06:51:13 +0200

    Implemented and registered a simple health check. Exposed the health check on API

---
+++ src/Common/Health/FooHealthCheck.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthChecksPrimer.Common.Health;
+
+/// <summary>
+/// A simple demo health check returning a randomly generated result
+/// </summary>
+public class FooHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new())
+    {
+        var foo = new Random().Next(10);
+
+        var data = new Dictionary<string, object> { { "foo", foo } };
+
+        if (foo > 4)
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy("Foo to high", data: data));
+        }
+
+        return Task.FromResult(HealthCheckResult.Healthy(data: data));
+    }
+}
