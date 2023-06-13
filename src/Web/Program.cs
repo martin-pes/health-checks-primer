5d23cac04c97b0bcf8a35072d4c628d386cbbfd3
martin-pes <50988072+martin-pes@users.noreply.github.com>
2023-06-13 07:32:50 +0200

    Customized output returned by health check endpoint (#5)
    Co-authored-by: Martin Pes <pes@cngroup.dk>

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,5 +1,6 @@
 using HealthChecksPrimer.Common.Health;
 using HealthChecksPrimer.Common.Services;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,17 @@ var app = builder.Build();
 
 app.MapGet("/", () => $"Health Check Primer, environment={builder.Environment.EnvironmentName}");
 app.MapPost("/stock/{symbol}", (string symbol, IStockService stockService) => stockService.Process(symbol));
-app.MapHealthChecks("/health");
+
+
+var healthCheckOptions = new HealthCheckOptions
+{
+    ResponseWriter = (context, healthReport) =>
+    {
+        context.Response.ContentType = "application/json; charset=utf-8";
+        return context.Response.WriteAsync(healthReport.ToCustomJson());
+    }
+};
+
+app.MapHealthChecks("/health", healthCheckOptions);
 
 app.Run();
