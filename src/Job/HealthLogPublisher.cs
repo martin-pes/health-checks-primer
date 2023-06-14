335a6204c74b9f09cc90fb4d2f36737bc1674140
martin-pes <50988072+martin-pes@users.noreply.github.com>
2023-06-14 07:07:35 +0200

    Added health checks to the job (#8)
    Co-authored-by: Martin Pes <pes@cngroup.dk>

---
+++ src/Job/HealthLogPublisher.cs
@@ -0,0 +1,28 @@
+﻿using HealthChecksPrimer.Common.Health;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthChecksPrimer.Job;
+
+public class HealthLogPublisher : IHealthCheckPublisher
+{
+    public HealthLogPublisher(ILogger<HealthLogPublisher> log)
+    {
+        _log = log;
+    }
+
+    public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+    {
+        var mapping = new Dictionary<HealthStatus, LogLevel>
+        {
+            { HealthStatus.Healthy, LogLevel.Information },
+            { HealthStatus.Degraded, LogLevel.Warning },
+            { HealthStatus.Unhealthy, LogLevel.Error }
+        };
+
+        _log.Log(mapping[report.Status], "Health check completed {status}, details: {result}", report.Status, report.ToCustomJson());
+
+        return Task.CompletedTask;
+    }
+
+    private readonly ILogger<HealthLogPublisher> _log;
+}
--- src/Job/Program.cs
+++ src/Job/Program.cs
@@ -1,11 +1,27 @@
+using HealthChecksPrimer.Common.Health;
 using HealthChecksPrimer.Common.Services;
 using HealthChecksPrimer.Job;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         services.AddStockServices(context.Configuration);
         services.AddHostedService<Worker>();
+
+        services.AddHealthChecks()
+            .AddCheck<FooHealthCheck>("Foo check")
+            .AddUrlGroup(new Uri("https://pcna-strobl.azurewebsites.net/stock/MSFT"))
+            .AddAzureBlobStorage(o => o.ContainerName = "stocks"); // assumes BlobServiceClient registered in DI
+
+        services.Configure<HealthCheckPublisherOptions>(options =>
+        {
+            options.Delay = TimeSpan.FromSeconds(2);
+            options.Period = TimeSpan.FromSeconds(5);
+        });
+
+        services.AddSingleton<IHealthCheckPublisher, HealthLogPublisher>();
+
     })
     .Build();
 
